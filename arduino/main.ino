567b89f75dd492d96bd52b55d0d6b685e09e332b
amanjagdev <amankumarjagdev@gmail.com>
2019-11-16 21:52:28 +0530

    made changes to loop

--- arduino/main.ino
+++ arduino/main.ino
@@ -31,7 +31,26 @@ void loop()
 
   Serial.println(distance);
     delay(40);
-  
-
+    val = digitalRead(sensor);   // read sensor value
+    if (val == HIGH) {           // check if the sensor is HIGH
+      digitalWrite(led, HIGH);   // turn LED ON
+      delay(100);                // delay 100 milliseconds 
+      
+      if (state == LOW) {
+        Serial.println("Motion detected!"); 
+        state = HIGH;       // update variable state to HIGH
+      }
+    } 
+    else {
+        digitalWrite(led, LOW); // turn LED OFF
+        delay(200);             // delay 200 milliseconds 
+        
+        if (state == HIGH){
+          Serial.println("Motion stopped!");
+          state = LOW;       // update variable state to LOW
+      }
   }
+  }
+
+
 
