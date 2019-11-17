a65d7b53593a0afa2494802110c6bd0d968d46a8
[Siddhant] <[siddhantagggarwal@gmail.com]>
2019-11-17 12:01:15 +0530

    Wolf

--- python/main.py
+++ python/main.py
@@ -1,35 +1,20 @@
 import wolframalpha 
-import speech_recognition as sr 
-
-r = sr.Recognizer()
-
-r.dynamic_energy_threshold = True
-
-r.pause_threshold = 2
-
-with sr.Microphone() as source:
-    audio = r.listen(source)
-
-try:
-    text = r.recognise_google(audio)
-
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-    return(0)
-
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
-question = text
-
-app_id = "PX2T6G-L2XQ4H9TE3" #wolfram-alpha id
-
+import serial
+import pyttsx3
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\MSSpeech_TTS_en-AU_Hayley')
+rate = engine.getProperty('rate')
+volume = engine.getProperty('volume')
+voice = engine.getProperty('voice')
+engine.setProperty('rate', 160)
+engine.say('Hi! I am from Wolframalpha! I can answer your each and every question')
+engine.runAndWait()
+question = input("Enter the Text")
+app_id = "PX2T6G-L2XQ4H9TE3" 
 client = wolframalpha.Client(app_id) 
-  
-res = client.query(question) 
-  
-# Includes only text from the response 
-answer = next(res.results).text 
-  
-print(answer) 
+res = client.query(question)  
+answer = next(res.results).text
+engine.say(answer)
+engine.runAndWait()
+print(answer)
