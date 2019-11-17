76f21051d5ceafe7879002c5df4b39ebbfe380b8
amanjagdev <amankumarjagdev@gmail.com>
2019-11-17 08:11:20 +0530

    modified code for nodemcu

--- arduino/node.ino
+++ arduino/node.ino
@@ -9,3 +9,23 @@ char auth[] = "pdL2Y6nuD5PEfAdzvXPk3CjrUYvhP9am";
 char ssid[] = "Noone";
 char pass[] = "qwertyuiop";
  
+#define DHTPIN 0          // D3
+
+WidgetLCD lcd(V1);
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+ 
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, t);
+  Blynk.virtualWrite(V6, h);
+}
