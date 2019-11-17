23a2a81c0b7499e8ae68f0382ff9a1fe672af20f
amanjagdev <amankumarjagdev@gmail.com>
2019-11-17 08:10:53 +0530

    added code fofr node mcu

---
+++ arduino/node.ino
@@ -0,0 +1,11 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#define TRIGGERPIN D1
+#define ECHOPIN    D2
+#include <DHT.h>
+ 
+char auth[] = "pdL2Y6nuD5PEfAdzvXPk3CjrUYvhP9am";
+ 
+char ssid[] = "Noone";
+char pass[] = "qwertyuiop";
+ 
